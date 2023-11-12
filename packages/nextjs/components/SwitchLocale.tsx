89b6e4d66fea9168b3cd7df85cd63545a31a2fa1
Han Tuzun <hantuzun@users.noreply.github.com>
2023-11-12 17:38:21 +0300

    Update language switcher

--- packages/nextjs/components/SwitchLocale.tsx
+++ packages/nextjs/components/SwitchLocale.tsx
@@ -10,21 +10,7 @@ export const SwitchLocale = ({ className }: { className?: string }) => {
   };
 
   return (
-    <div className={`flex flex-row items-center gap-2 bg-slate-800 rounded-lg px-3 justify-center h-12 ${className}`}>
-      <button
-        className={`h-6 w-6 text-lg flex items-center justify-center ${router.locale === "en" && "text-slate-400"}`}
-        onClick={() => toggle("en")}
-      >
-        EN
-      </button>
-
-      <div className="text-lg flex items-center justify-center border w-0 h-6 border-white"></div>
-      <button
-        className={`h-6 w-6 text-lg flex items-center justify-center ${router.locale === "tr" && "text-slate-400"}`}
-        onClick={() => toggle("tr")}
-      >
-        TR
-      </button>
+    <div className={`flex flex-row items-center gap-2 bg-slate-800 rounded-full px-3 justify-center h-12 ${className}`}>
       <div className="bg-slate-500 rounded-full p-1">
         <svg
           stroke="currentColor"
@@ -39,6 +25,23 @@ export const SwitchLocale = ({ className }: { className?: string }) => {
           <path d="M478.33 433.6l-90-218a22 22 0 00-40.67 0l-90 218a22 22 0 1040.67 16.79L316.66 406h102.67l18.33 44.39A22 22 0 00458 464a22 22 0 0020.32-30.4zM334.83 362L368 281.65 401.17 362zm-66.99-19.08a22 22 0 00-4.89-30.7c-.2-.15-15-11.13-36.49-34.73 39.65-53.68 62.11-114.75 71.27-143.49H330a22 22 0 000-44H214V70a22 22 0 00-44 0v20H54a22 22 0 000 44h197.25c-9.52 26.95-27.05 69.5-53.79 108.36-31.41-41.68-43.08-68.65-43.17-68.87a22 22 0 00-40.58 17c.58 1.38 14.55 34.23 52.86 83.93.92 1.19 1.83 2.35 2.74 3.51-39.24 44.35-77.74 71.86-93.85 80.74a22 22 0 1021.07 38.63c2.16-1.18 48.6-26.89 101.63-85.59 22.52 24.08 38 35.44 38.93 36.1a22 22 0 0030.75-4.9z"></path>
         </svg>
       </div>
+      <button
+        className={`h-6 w-6 text-lg flex items-center justify-center ${
+          router.locale === "en" && "text-slate-400 cursor-default"
+        }`}
+        onClick={() => toggle("en")}
+      >
+        EN
+      </button>
+      <div className="text-lg flex items-center justify-center border w-0 h-6 border-slate-400"></div>
+      <button
+        className={`h-6 w-6 mr-2 text-lg flex items-center justify-center ${
+          router.locale === "tr" && "text-slate-400 cursor-default"
+        }`}
+        onClick={() => toggle("tr")}
+      >
+        TR
+      </button>
     </div>
   );
 };
