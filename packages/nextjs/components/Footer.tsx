ed1a633b461a2e36ea62edcb3cca84fa5b9f0182
Han Tuzun <hantuzun@users.noreply.github.com>
2023-11-12 17:49:01 +0300

    Update the footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { hardhat } from "viem/chains";
 import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { HeartIcon } from "@heroicons/react/24/outline";
 import { SwitchLocale } from "~~/components/SwitchLocale";
-import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
 import { Faucet } from "~~/components/scaffold-eth";
 import { useGlobalState } from "~~/services/store/store";
 import { getTargetNetwork } from "~~/utils/scaffold-eth";
@@ -46,29 +44,40 @@ export const Footer = () => {
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
             <div className="text-center">
-              <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
-                Fork me
+              <a
+                href="https://app.gitter.im/#/room/%23kilim:gitter.im"
+                target="_blank"
+                rel="noreferrer"
+                className="link"
+              >
+                Workshop Support on Gitter
               </a>
             </div>
             <span>·</span>
             <div className="flex justify-center items-center gap-2">
-              <p className="m-0 text-center">
-                Built with <HeartIcon className="inline-block h-4 w-4" /> at
-              </p>
+              <p className="m-0 text-center">Built on top of</p>
               <a
                 className="flex justify-center items-center gap-1"
                 href="https://buidlguidl.com/"
                 target="_blank"
                 rel="noreferrer"
               >
-                <BuidlGuidlLogo className="w-3 h-5 pb-1" />
                 <span className="link">BuidlGuidl</span>
+              </a>
+              <p className="m-0 text-center">by</p>
+              <a
+                className="flex justify-center items-center gap-1"
+                href="https://neu.fi/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="link">Neufi</span>
               </a>
             </div>
             <span>·</span>
             <div className="text-center">
-              <a href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA" target="_blank" rel="noreferrer" className="link">
-                Support
+              <a href="http://github.com/neu-fi/kilim" target="_blank" rel="noreferrer" className="link">
+                Fork me on GitHub
               </a>
             </div>
           </div>
