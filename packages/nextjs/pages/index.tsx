fb8f294f0e98250b070ceb85509d03917920a66f
koybasimuhittin <koybasimuhittin@gmail.com>
2023-11-12 16:49:08 +0300

    fix markdown styles

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -99,7 +99,10 @@ const Home: NextPage = ({ markdownData }: any) => {
         </div>
       </section>
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="p-4" dangerouslySetInnerHTML={{ __html: markdownData.contentHtml }} />
+        <div
+          className="max-w-screen-xl px-8 py-8 mx-auto prose lg:prose-xl"
+          dangerouslySetInnerHTML={{ __html: markdownData.contentHtml }}
+        />
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
