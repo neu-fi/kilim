ff3b8afb5122a3fbbe54dd2bc795c4f16cfadc33
lil reentrant <cagindonmez@gmail.com>
2023-11-12 11:59:30 +0000

    Fixed file paths

--- packages/nextjs/setup_env.sh
+++ packages/nextjs/setup_env.sh
@@ -4,9 +4,9 @@ PREPEND_STRING="https://8545-"
 DOT="."
 
 # Check if .env.example exists
-if [ -f ".env.example" ]; then
+if [ -f "packages/nextjs/.env.example" ]; then
     # Copy .env.example to .env.local
-    cp .env.example .env.local
+    cp packages/nextjs/.env.example packages/nextjs/.env.local
     echo ".env.example copied to .env.local"
 
     # Check if GITPOD_RPC_URL is set in the environment
@@ -18,10 +18,10 @@ if [ -f ".env.example" ]; then
         FULL_URL="${PREPEND_STRING}${HOSTNAME}${DOT}${GITPOD_WORKSPACE_CLUSTER_HOST}"
 
         # Append NEXT_GITPOD_RPC_URL to .env.local
-        echo -e "\nNEXT_PUBLIC_GITPOD_RPC_URL=$FULL_URL" >> .env.local
+        echo -e "\nNEXT_PUBLIC_GITPOD_RPC_URL=$FULL_URL" >> packages/nextjs/.env.local
         echo "NEXT_PUBLIC_GITPOD_RPC_URL set in .env.local"
     fi
 else
-    echo ".env.example not found. Please ensure it exists in the current directory."
+    echo "packages/nextjs/.env.example not found. Please ensure it exists in the current directory."
     exit 1
 fi
